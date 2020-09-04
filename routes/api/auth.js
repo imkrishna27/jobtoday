090da694374ea6f5061c4a444731028bc4987910
imkrishna <krishna.mymail17@gmail.com>
2020-09-04 12:35:59 +0530

    secondcommit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/users');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const { check, validationResult } = require('express-validator');
 
 // @route GET api/auth
 // @desc TEST route
@@ -16,5 +21,51 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route POST api/auth
+// @desc login
+// @access Public
+
+router.post(
+  '/',
+  [
+    check('email', 'Enter valid E-mail').isEmail(),
+    check('password', 'Password Required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: ' Wrong Email' }] });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return res.status(400).json({ errors: [{ msg: 'Wrong Password!' }] });
+      }
+
+      const payload = { user: { id: user.id } };
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
