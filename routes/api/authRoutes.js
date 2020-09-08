09ad3a5c9128a6b8195db61b1b703036c7f39cb5
Deepak <deepak12.94k@gmail.com>
2020-09-09 03:56:03 +0530

    few changes jwt

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 module.exports = (app) => {
     app.get('/auth/google', 
         passport.authenticate('google',{
@@ -9,14 +10,32 @@ module.exports = (app) => {
 
     app.get('/auth/google/callback',
     passport.authenticate('google'),
-    // (req,res) => {
-    // }
-    );
-    
+    (req,res) => {
+      const payload = { user: {id: req.user.id }};
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    }
+  );
+};   
 //testinf google oauth users
-
-    app.get('/api/current_user',(req, res) => {
-        res.send(req.user);
-    });
+    // app.get('/api/current_user',(req, res) => {
+        // res.send(res.user);
+      // const payload = { user: {id: req.user.id }};
+      // jwt.sign(
+      //   payload,
+      //   config.get('jwtToken'),
+      //   { expiresIn: 360000 },
+      //   (err, token) => {
+      //     if (err) throw err;
+      //     res.json({ token });
+      //   }
+      // );
+    // });
 //////////////////////////////
-};
