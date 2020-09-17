ff8c17163b0440162c4dd439386159ee8a25091b
Deepak <deepak12.94k@gmail.com>
2020-09-17 18:58:02 +0530

    add forget password

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,9 +5,12 @@ const User = require('../../models/users');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const keys = require('../../config/dev');
 // const passport = require('passport');
 const { check, validationResult } = require('express-validator');
 const { route } = require('./users');
+const sgMail = require('@sendgrid/mail');         // run "npm install --save @sendgrid/mail"
+sgMail.setApiKey(keys.mail_key);
 
 // @route GET api/auth
 // @desc TEST route
@@ -68,5 +71,87 @@ router.post(
     }
   }
 );
+
+router.post('/forgetpassword',
+  [
+    check('email')
+      .not()
+      .isEmpty()
+      .isEmail()
+      .withMessage('Must be a valid Email!')
+  ],
+  async(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array()});
+    }
+
+    const{ email } = req.body;
+    try{
+      const isemail = await User.findOne({ email });
+      console.log(isemail);
+      console.log(keys.jwt_reset_token);
+      if(!isemail){
+        return res.status(400).json({ errors: [{ msg: 'Wrong Email' }] });
+      }else{
+        const emailToken = jwt.sign(
+          {
+            isemail: {id: isemail.id}
+          },
+          keys.jwt_reset_token,
+          {
+            expiresIn: '1m'
+          }
+        );
+
+          const emailData = {
+            from: keys.emailFrom,
+            to: email,
+            subject: 'Password Reset Link',
+            html: `
+                    <h1>Please use the following link to reset your password</h1>
+                    <p>${keys.client_url}/users/password/reset/${emailToken}</p>
+                    <hr />
+                    <P> this email contain sensetive info</p>
+                    <p>${keys.client_url}</p>
+                  `
+          };
+
+          return isemail.updateOne({
+            resetPasswordLink: emailToken
+          },
+          (err, sucess) => {
+            if(err) {
+              console.log('RESET PASSWORD LINK ERROR', err);
+              return res.status(400).json({
+                error:'database connection error on user password forget'
+              });
+            } else {
+              sgMail
+                .send(emailData)
+                .then(sent => {
+                  console.log('email sent', sent);
+                  return res.json({
+                    message: `Email has been sent to ${ email }. Follow the instruction`
+                  });
+                })
+                .catch(err => {
+                  console.log('Email error',err);
+                  return res.json({
+                    message: err.message
+                  });
+                });
+            }
+          }  
+        );
+      }
+      
+    } catch(err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 
 module.exports = router;
